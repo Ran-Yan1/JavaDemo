cf97beb17c724257ca60f11858cde91eb6a902a0
Ran-Yan <2490782859@qq.com>
2022-02-21 16:02:11 +0800

    鸟

---
+++ src/Bird.java
@@ -0,0 +1,11 @@
+/**
+ *测试多态性
+ **/
+public class Bird {
+    public void tweet(){
+        System.out.println("jiu~jiu~");
+    }
+    public void fly(int a,int b){
+        System.out.println("I am flying from "+a+" to "+b);
+    }
+}
